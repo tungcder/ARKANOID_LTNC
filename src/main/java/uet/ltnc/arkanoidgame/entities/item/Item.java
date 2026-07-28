0c2d07650dda1afd1edf55e5d2929ca222cd0ba0
Trieu Tuan Thanh <trieutuanthanh2611@gmail.com>
2026-07-29 06:35:01 +0700

    thêm base item

---
+++ src/main/java/uet/ltnc/arkanoidgame/entities/item/Item.java
@@ -0,0 +1,87 @@
+package uet.ltnc.arkanoidgame.entities.item;
+
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
+import uet.ltnc.arkanoidgame.entities.ball.Ball;
+import uet.ltnc.arkanoidgame.entities.paddle.Paddle;
+import uet.ltnc.arkanoidgame.utils.GameConstants;
+
+public abstract class Item {
+
+    private double x;
+    private double y;
+
+    private final double width;
+    private final double height;
+
+    private double fallSpeed;
+    private boolean collected;
+
+    protected Item(double x, double y) {
+        width = 30;
+        height = 30;
+
+        this.x = x - width / 2;
+        this.y = y;
+
+        fallSpeed = 2;
+        collected = false;
+    }
+
+    public void update() {
+        if (!collected) {
+            y += fallSpeed;
+        }
+    }
+
+    public void render(GraphicsContext gc) {
+        if (collected) {
+            return;
+        }
+
+        gc.setFill(getColor());
+        gc.fillRect(x, y, width, height);
+
+        gc.setStroke(Color.WHITE);
+        gc.strokeRect(x, y, width, height);
+    }
+
+    public boolean collidesWith(Paddle paddle) {
+        return !collected
+                && x < paddle.getX() + paddle.getWidth()
+                && x + width > paddle.getX()
+                && y < paddle.getY() + paddle.getHeight()
+                && y + height > paddle.getY();
+    }
+
+    public void collect(Paddle paddle, Ball ball) {
+        if (collidesWith(paddle)) {
+            apply(paddle, ball);
+            collected = true;
+        }
+    }
+
+    public boolean isActive() {
+        return !collected && y <= GameConstants.HEIGHT;
+    }
+
+    protected abstract Color getColor();
+
+    public abstract void apply(Paddle paddle, Ball ball);
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+}
